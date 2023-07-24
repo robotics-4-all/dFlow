1ad39bdf4068515499c8fffc4ccdca9ca5418a29
harabalos <babismetaxas000@gmail.com>
2023-07-24 23:18:23 +0300

    can create an event

--- dflow/openapi_to_dflow.py
+++ dflow/openapi_to_dflow.py
@@ -1,12 +1,12 @@
 from intent_generator import *
 from jinja2 import Environment, FileSystemLoader
 
-def create_service_name(operationId):
-   return f'{operationId}_svc'
+def create_name(operationId, ending = None):
+   if ending == None:
+       return operationId
+   else:
+       return operationId + '_' + ending
     
-def create_intent_name(operationId):
-    return f'{operationId}'
-
 def create_service(service_name, verb, host, port, path):
     file_loader = FileSystemLoader('templates') 
     env = Environment(loader=file_loader)
@@ -15,18 +15,24 @@ def create_service(service_name, verb, host, port, path):
     output = template.render(service_name=service_name, verb=verb, host=host, port=port, path=path)
     return output
 
-def create_intents(name, triggers):
+def create_triggers(intent_name, event_name, triggers):
     file_loader = FileSystemLoader('templates') 
     env = Environment(loader=file_loader)
     template = env.get_template('grammar-templates/triggers.jinja')
 
     phrases = generate_intent_examples(model,tokenizer,operation.summary)
-    trigger = {
+    intent = {
         "type": "Intent",
-        "name": name,  
+        "name": intent_name,  
         "phrases": phrases
     }
-    triggers = [trigger] 
+
+    event = {
+        "type": "Event",
+        "name": event_name,  
+        "uri": f"bot/event/{event_name}"
+    }
+    triggers = [intent,event] 
 
     output = template.render(triggers=triggers)
     return output
@@ -39,16 +45,19 @@ for endpoint in parsed_api:
     for operation in endpoint.operations:
 
         triggers = []  
-        service_name = create_service_name(operation.operationId)
-        intent_name = create_intent_name(operation.operationId)
+
+        service_name = create_name(operation.operationId, "svc")
+        intent_name = create_name(operation.operationId)
+        event_name = create_name(operation.operationId, "ev")
         verb = operation.type.upper() 
         host = fetchedApi["host"]
         port = fetchedApi.get("port", None)
         path = endpoint.path
 
         eservice_definition = create_service(service_name, verb, host, port, path)
-        intents = create_intents(intent_name,triggers)
+        triggers = create_triggers(intent_name,event_name,triggers)
 
         print(eservice_definition)
-        print(intents)
+        print(triggers)
+
 
