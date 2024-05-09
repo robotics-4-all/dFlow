24af19d82e1197fe9e423deb3bfe15dc4057bde4
Konstantinos Panayiotou <klpanagi@gmail.com>
2024-05-09 17:11:15 +0300

    Restore textx generator for rasa

--- dflow/__init__.py
+++ dflow/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.4.0"
 __author__ = "klpanagi"
 
 from .language import dflow_language
-# from .generator import dflow_generate_rasa, codegen as rasa_generator
+from .generator import dflow_generate_rasa
